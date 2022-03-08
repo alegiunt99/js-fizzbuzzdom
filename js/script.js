14c059d011f7815b7e618abc79675b32e223c1dc
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-08 16:19:44 +0100

    Ho aggiunto i numeri da 1 a 100 nella console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log('OK JS');
+
+// devo creare un loop che conta i numeri da 1 a 100
+// i numeri divisibili per 3, devo chiamarli "fizz"
+// i numeri divisibili per 5, devo chiamarli "buzz"
+// i numeri divisibili sia per 3 e per 5 devo chiamarli "fizzbuzz"
+const boxesContainer = document.querySelector(".container");
+
+for (let numberBasic = 1; numberBasic < 101; numberBasic++){
+
+    let boxesNumber = `<div id="number-${numberBasic}" class="numbers">${numberBasic}</div>`;
+
+    boxesContainer.innerHTML += boxesNumber;
+
+    console.log(numberBasic);
+
+    if (numberBasic % 3 === 0){
+        
+    }
+}
